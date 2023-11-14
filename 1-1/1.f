7d7411579f0dafc441f7cdf6e4b1c172df4b5bee
jOh <joaovitordelima81@usp.br>
2023-11-14 17:39:44 -0300

    mudei umas coisas para ficar mais facil de visualizar

--- 1-1/1.f
+++ 1-1/1.f
@@ -5,7 +5,7 @@ C       Variaveis
         dt = 0.00001
         g = 9.81
         t = 0
-        z = 100 !100m 
+        z = 100 !100m
         z_a = z
         v = 0
 
@@ -19,14 +19,14 @@ C
         z = z + v*dt
         t = t + dt
 
-C       z_a = z_a -g*(t**2)/2 
+C       z_a = z_a -g*(t**2)/2
 
 C       Salva os resultados
         write(1,7) t, z
 7       Format(F10.3, F10.3)
-        
+
         END DO
-C       
+C
         close(1)
 
         END PROGRAM TEST
