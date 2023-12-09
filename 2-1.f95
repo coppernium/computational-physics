99c08a1c4146725c4d07311dcb6666f760423473
Joao Vitor <joaovitordelima81@usp.br>
2023-12-09 19:41:25 -0300

    f

--- 2-1.f95
+++ 2-1.f95
@@ -1,7 +1,7 @@
         program bicicleta
             real*8 vel(1000), tempo(1000)
             call variaveis(m,P,dt,t,n,vel,tempo)
-            call calculo(m,P,dt,t,n,vel,tempo)
+            call calculo(m,P,dt,n,vel,tempo)
             call dados(n,vel,tempo)
         
         end program bicicleta
@@ -18,7 +18,7 @@
             return
         end subroutine variaveis
 
-        subroutine calculo(m,P,dt,t,n,vel,tempo)
+        subroutine calculo(m,P,dt,n,vel,tempo)
             real*8 vel(n), tempo(n)
             do i=1,n-1
                 vel(i+1) = vel(i) + (P*dt)/(m*vel(i))
@@ -29,10 +29,9 @@
 
         subroutine dados(n,vel,tempo)
             real*8 vel(n), tempo(n)
-            open(unit=1,file="C:\Users\joaov\Documents\computational-physics\dados1.dat")
+            open(unit=1,file="dados.dat")
             do i=1,n
-            write(1,7) tempo(i), vel(i)
-7           format(F10.3,", ", F10.3)
+                write(1,*) tempo(i), vel(i)
             end do 
             close(1)
         end subroutine dados
