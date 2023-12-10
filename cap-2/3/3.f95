8663ae76f475d4d4d6729ed7e1c89cfcf00e4385
Joao Vitor <joaovitordelima81@usp.br>
2023-12-10 08:20:35 -0300

    exercicio 3

--- cap-2/3/3.f95
+++ cap-2/3/3.f95
@@ -1,12 +1,12 @@
         program main
-            real*8 vel(100000), tempo(100000)
-            call variaveis(m,P,h,t,A,eta,C,rho,dt,n,vel,tempo)
-            call calculo(m,P,h,A,eta,C,rho,dt,n,vel,tempo)
-            call dados(n,vel,tempo)
+            real*8 vel(100000), tempo(100000), fat1(100000), fat2(100000)
+            call variaveis(m,P,h,t,A,eta,C,rho,dt,n,vel,tempo,fat1,fat2)
+            call calculo(m,P,h,A,eta,C,rho,dt,n,vel,tempo,fat1,fat2)
+            call dados(n, vel, fat1, fat2)
         end program main
 
-        subroutine variaveis(m,P,h,t,A,eta,C,rho,dt,n,vel,tempo)
-            real*8 vel(i), tempo(1)
+        subroutine variaveis(m,P,h,t,A,eta,C,rho,dt,n,vel,tempo,fat1,fat2)
+            real*8 vel(1), tempo(1), fat1(1), fat2(1)
             eta = 2*10**(-5)
             A = 0.33
             P = 400
@@ -17,25 +17,29 @@
             dt = 0.1
             t = 300
             n = int(t/dt)
-            vel(1) = 40
+            vel(1) = 4
             tempo(1) = 0
+            fat1(1) = 0
+            fat2(1) = 0
             return
         end subroutine variaveis
 
-        subroutine calculo(m,P,h,A,eta,C,rho,dt,n,vel,tempo)
-            real*8 vel(n), tempo(n)
-            do i=1,n
+        subroutine calculo(m,P,h,A,eta,C,rho,dt,n,vel,tempo,fat1,fat2)
+            real*8 vel(n), tempo(n), fat1(n), fat2(n)
+            do i=1,n-1
                 vel(i+1) = vel(i) + (P*dt)/(m*vel(i)) - (C*rho*A*(vel(i)**2)*dt)/(2*m) - eta*A*vel(i)*dt/(h*m)
+                fat1(i+1) = fat1(i) + (C*rho*A*(vel(i)**2)*dt)/(2*m)
+                fat2(i+1) =fat2(i) + eta*A*vel(i)*dt/(h*m)
                 tempo(i+1) = tempo(i) + dt
             end do
         end subroutine calculo
 
-        subroutine dados(n,vel,tempo)
-            real*8 vel(n), tempo(n)
-            open(unit=1, file="dados1.dat")
+        subroutine dados(n, vel, fat1, fat2)
+            real*8 vel(n), fat1(n), fat2(n)
+            open(unit=1, file="atrito_comp.dat")
             do i=1,n-1
-                write(1,7) tempo(i), vel(i)
-7               format(F10.3, ",", F10.3)
+                write(1,7) vel(i), fat1(i), fat2(i)
+7               format(F10.3, ",", F10.3, ",", F10.3)
             end do
             close(1)
         end subroutine dados
--- cap-2/3/plot-3.py
+++ cap-2/3/plot-3.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dados = pd.read_csv(r"C:\Users\joaov\Documents\computational-physics\cap-2\3\dados1.dat", header=None, sep=",")
+dados = pd.read_csv(r"C:\Users\joaov\Documents\computational-physics\cap-2\3\atrito_comp.dat", header=None, sep=",")
 x = dados[0]
 y = dados[1]
+y2 = dados[2]
 
-plt.xlim(0,300)
-plt.ylim(0,50)
-plt.plot(x,y)
+#plt.xlim(0,max(x))
+#plt.ylim(0,200)
+plt.plot(x,y,label="v^2")
+plt.plot(x,y2,label="v")
+plt.xlabel("Velocity [m/s]")
+plt.ylabel("Work done by friction [W]")
+plt.legend(loc='best')
 plt.show()
