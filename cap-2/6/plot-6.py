1069b72e428ba2de58eb16860fb1fe3eb9a437fd
jOh <joaovitordelima81@usp.br>
2023-12-15 11:23:34 -0300

    mudei o nome da

--- cap-2/6/plot-6.py
+++ cap-2/6/plot-6.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dados = pd.read_csv(r"C:\Users\joaov\Documents\computational-physics\cap-2\6\data1.data", header=None, sep=",")
+dados = pd.read_csv("data1.data", header=None, sep=",")
 x = dados[3]
 y = dados[4]
 plt.xlim(0,4000)
