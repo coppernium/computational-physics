99b6983dca81cd5887d3b3208cbbe660df84b70e
jOh <joaovitordelima81@usp.br>
2023-12-20 08:34:51 -0300

    codigo semi pronto

--- cap-2/8/8.f95
+++ cap-2/8/8.f95
@@ -1,5 +1,9 @@
 program main
 real*8 posx(100000), posy(100000), temp(100000)
+
+call init_variables(dt,B,n,posx,posy,temp)
+call calc_euler(dt,B,n,posx,posy,temp)
+call save_data(n,posx,posy)
 end program main
 
 subroutine init_variables(dt,B,n,posx,posy,temp)
@@ -20,6 +24,29 @@ subroutine calc_euler(dt,B,n,posx,posy,temp)
   pi = acos(-1.00)
   velx(1) = 700*cos(pi/4)
   vely(1) = 700*sin(pi/4)
+  g = 9.81
   do i=1,n-1
+    ! Atrito
+    Fat = -B*sqrt(velx(i)**2 + vely(i)**2)
 
+    ! Calc x
+    velx(i+1) = velx(i) + Fat*velx(i)*dt
+    posx(i+1) = posx(i) + velx(i)*dt
+    ! Calc y
+    vely(i+1) = vely(i) + (-g + Fat*vely(i))*dt
+    posy(i+1) = posy(i) + vely(i)*dt
+    ! Atualiza o tempo
+    temp(i+1) = temp(i) + dt
+  end do
 end subroutine calc_euler
+
+subroutine save_data(n,posx,posy)
+  real*8 posx(n), posy(n)
+
+  open(UNIT=1, FILE="dados.data")
+  do i =1,n-1
+    write(1,7) posx(i), posy(i)
+7   format(F10.3, ",", F10.3)
+  end do
+  close(1)
+end subroutine save_data
