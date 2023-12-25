9971d7fa394d54353f43672f5dcc6b1de3632096
jOh <joaovitordelima81@usp.br>
2023-12-25 10:22:43 -0300

    código da 17

--- cap-2/17/17.f95
+++ cap-2/17/17.f95
@@ -1,3 +1,58 @@
 program main
   real*8 posx(100000), posy(100000), posz(100000)
+
+  call initialize(dt,n,S,posx,posy,posz)
+  call calc_euler(dt,n,S,posx,posy,posz)
+  call dados(n,posx,posy,posz)
 end program main
+
+subroutine initialize(dt,n,S,posx,posy,posz)
+  real*8 posx(1), posy(1), posz(1)
+  t_sim = 300
+  dt 0.01
+  n=t_sim/dt
+  S = 0.00041
+  posx(1)=0
+  posy(1)=0
+  posz(1)=0
+end subroutine initialize
+
+subroutine calc_euler(dt,n,S,posx,posy,posz)
+  real*8 posx(n), posy(n), posz(n), velx(n), vely(n), velz(n)
+  ! Váriaveis para o cálculo
+  g = 9.81
+  pi = acos(-1.00)
+  velx(1) = 0
+  vely(1) = 31.29
+  velz(1) = 0
+  w = 188.495
+
+  do i=1,n-1
+    ! Variaveis variaveis
+    velt = sqrt(velx(i)**2 + vely(i)**2 + velz(i)**2)
+    B = 0.0039 + 0.0058/(1+exp((velt-35)/5))
+    fat = -B*velt
+    ! Calculo x
+    velx(i+1) = velx(i) - S*velx(i)*w*dt
+    posx(i+1) = posx(i) + velx(i)*dt
+
+    ! Calculo y
+    vely(i+1) = vely(i) + fat*vely(i)*dt
+    posy(i+1) = posy(i) + vely(i)*dt
+
+    ! Calculo z
+    velz(i+1) = velz(i) - g*dt + fat*velz(i)*dt
+    posz(i+1) = posz(i) + velz(i)*dt
+  end do
+
+end subroutine calc_euler
+
+subroutine dados(n,posx,posy,posz)
+  real*8 posx(n), posy(n), posz(n)
+  open(UNIT=1, file="dados1.data")
+  do i=1,n-1
+    write(1,7) posx(i), posy(i)
+7   format(F10.3, ",", F10.3)
+  end do
+  close(1)
+end subroutine dados
